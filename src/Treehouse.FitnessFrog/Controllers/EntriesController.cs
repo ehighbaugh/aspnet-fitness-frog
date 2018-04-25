f71ba2dde50c68a4b08be7bbdb86edfdfb72eab5
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-04-24 21:37:00 -0400

    Added confirmation messages for adding, updating and deleting entries

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -61,6 +61,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.AddEntry(entry);
 
+                TempData["Message"] = "Your entry was successfully added";
+
                 return RedirectToAction("Index");
             }
 
@@ -96,6 +98,9 @@ namespace Treehouse.FitnessFrog.Controllers
             if (ModelState.IsValid)
             {
                 _entriesRepository.UpdateEntry(entry);
+
+                TempData["Message"] = "Your entry was successfully updated";
+
                 return RedirectToAction("Index");
             }
 
@@ -124,6 +129,9 @@ namespace Treehouse.FitnessFrog.Controllers
         public ActionResult Delete(int id)
         {
             _entriesRepository.DeleteEntry(id);
+
+            TempData["Message"] = "Your entry was successfully deleted";
+
             return RedirectToAction("Index");
         }
 
